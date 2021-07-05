82bb9edc33a058228c27c16d151b21283d299ffe
Luca Denhez <48540043+lucadenhez@users.noreply.github.com>
2021-07-04 17:30:20 -0700

    Add files via upload

---
+++ Client/Server Control/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Haha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.responses = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ipBox = new System.Windows.Forms.TextBox();
+            this.link = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(305, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Shutdown";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(32, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server Control";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // responses
+            // 
+            this.responses.AutoSize = true;
+            this.responses.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.responses.ForeColor = System.Drawing.Color.Coral;
+            this.responses.Location = new System.Drawing.Point(68, 354);
+            this.responses.Name = "responses";
+            this.responses.Size = new System.Drawing.Size(230, 15);
+            this.responses.TabIndex = 3;
+            this.responses.Text = "Enter the IP and click connect when ready!";
+            this.responses.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(305, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Monke";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(305, 30);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Close Minecraft";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(219, 31);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Connect";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // ipBox
+            // 
+            this.ipBox.Location = new System.Drawing.Point(237, 316);
+            this.ipBox.Name = "ipBox";
+            this.ipBox.Size = new System.Drawing.Size(112, 20);
+            this.ipBox.TabIndex = 7;
+            this.ipBox.Text = "192.168.86.1234";
+            this.ipBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // link
+            // 
+            this.link.Location = new System.Drawing.Point(29, 213);
+            this.link.Name = "link";
+            this.link.Size = new System.Drawing.Size(224, 20);
+            this.link.TabIndex = 9;
+            this.link.Text = "https://example.com/test.exe";
+            this.link.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(259, 213);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 20);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Download";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(29, 253);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(305, 28);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Stop Recieving";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(361, 382);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.link);
+            this.Controls.Add(this.ipBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.responses);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.99D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Server Control";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label responses;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox ipBox;
+        private System.Windows.Forms.TextBox link;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Client/Server Control/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Haha
+{
+    public partial class Form1 : Form
+    {
+        TcpClient tcpClient;
+        NetworkStream myns;
+        StreamWriter mysw;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendCommand("shutdown");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SendCommand("monke");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SendCommand("minecraft");
+        }
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SendCommand(link.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SendCommand("stop");
+        }
+
+        void SendCommand(string command)
+        {
+            try
+            {
+                if (tcpClient != null)
+                {
+                    myns = tcpClient.GetStream();
+                    mysw = new StreamWriter(myns);
+
+                    Thread.Sleep(50);
+
+                    mysw.WriteLine(command);
+
+                    mysw.Flush();
+
+                    responses.Text = "Sent command!";
+                }
+                else
+                {
+                    MessageBox.Show("TCP Client hasn't been started yet.");
+                }
+            }
+            catch(Exception ex)
+            {
+                responses.Text = "Failed to send command.";
+                MessageBox.Show("Failed to send command:\n\n" + ex.ToString());
+
+                mysw.Flush();
+            }
+        }
+
+        bool Connect(string IP, int port)
+        {
+            try
+            {
+                tcpClient = new TcpClient(IP, port);
+
+                int size = Marshal.SizeOf((uint)0);
+
+                byte[] keepAlive = new byte[size * 3];
+
+                Buffer.BlockCopy(BitConverter.GetBytes((uint)1), 0, keepAlive, 0, size);
+                Buffer.BlockCopy(BitConverter.GetBytes((uint)5000), 0, keepAlive, size, size);
+                Buffer.BlockCopy(BitConverter.GetBytes((uint)5000), 0, keepAlive, size * 2, size);
+
+                tcpClient.Client.IOControl(IOControlCode.KeepAliveValues, keepAlive, null);
+
+                return true;
+            }
+            catch (SocketException ex) {
+                responses.Text = "Failed to connect! Please try again.";
+                MessageBox.Show("Failed to connect! The connection either refused or timed out.\n\n" + ex.ToString());
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                responses.Text = "An unknown error occured.";
+                MessageBox.Show("An unknown error occured:\n\n" + ex.ToString());
+
+                return false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string IP = ipBox.Text;
+
+            responses.Text = "Starting...";
+
+            bool status = Connect(IP, 5020);
+
+            if (status == true)
+            {
+                responses.Text = "Started TCP client!";
+            }
+            else
+            {
+                responses.Text = "Failed to start TCP client!";
+            }
+            Thread.Sleep(1000);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (tcpClient != null)
+            {
+                myns.Close();
+                mysw.Close();
+                tcpClient.Close();
+            }
+        }
+    }
+}
